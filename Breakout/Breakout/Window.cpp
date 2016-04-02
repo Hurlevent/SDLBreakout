f0580b59bb776a609b48594ec58e228e211df2c3
Hurlevent <Olivereftevaag@gmail.com>
2016-04-02 15:11:13 +0200

    I have to commit in order to merge

--- Breakout/Breakout/Window.cpp
+++ Breakout/Breakout/Window.cpp
@@ -87,6 +87,9 @@ namespace Breakout {
 
 		SDL_Quit();
 	}
-	
 
+	void Window::set_render_draw_color(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+	{
+
+	}
 }
--- Breakout/Breakout/Window.h
+++ Breakout/Breakout/Window.h
@@ -11,6 +11,7 @@ namespace Breakout {
 		~Window();
 		int get_height() { return _height; }
 		int get_width() { return _width; }
+		void set_render_draw_color(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
 	private:
 		int _width;
 		int _height;
