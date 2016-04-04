83ed2d1478ccf875d653fae34aeb6675bc7634d2
Oliver Eftevaag <eftoli14@student.westerdals.no>
2016-04-04 17:40:01 +0200

    Ok, didn|t work last time. Time to try again

--- SDLBreakout/SDLBreakout/Main.cpp
+++ SDLBreakout/SDLBreakout/Main.cpp
@@ -7,8 +7,8 @@
 //
 #ifdef _WIN32
 #include <SDL.h>
-#include <SDL2_ttf.h>
-#include <SDL2_image.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
 #elif __APPLE__
 #include <SDL2/SDL.h>
 #include <SDL2_ttf/SDL_ttf.h>
